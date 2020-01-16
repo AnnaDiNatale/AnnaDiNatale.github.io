99d338c7e23d1317a083b1e319eeb90c3eddfbc4
Anna Di Natale <47451033+AnnaDiNatale@users.noreply.github.com>
2020-01-16 17:07:28 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,9 +30,11 @@ function getTaskOutputs(i) {
 }
 
 function updateTask() {
-    console.log('updateTask');
+    console.log('updateTask1');
     console.log($("input[name=Options]:checked").val());
     custom.showTask(getTaskInputs(state.taskIndex), state.taskIndex, getTaskOutputs(state.taskIndex));
+    console.log('updateTask2');
+    console.log($("input[name=Options]:checked").val());
     $("#progress-bar").progress("set progress", state.taskIndex + 1);
     if (state.taskIndex == config.meta.numSubtasks - 1) {
         $("#next-button").addClass("disabled");
@@ -54,6 +56,8 @@ function updateTask() {
         $("#submit-button").addClass("disabled");
         $("#final-task-fields").css("display", "none");
     }
+    console.log('updateTask3');
+    console.log($("input[name=Options]:checked").val());
 }
 
 function nextTask() {
