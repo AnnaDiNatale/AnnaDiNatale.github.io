1a2373ab2b0ce63be3fa5d78ddb4baa75cb8616e
Anna Di Natale <47451033+AnnaDiNatale@users.noreply.github.com>
2020-01-16 16:57:59 +0100

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -49,7 +49,7 @@ var custom = {
          *   config.meta.aggregate is true, an object with key-value pairs to be merged with the
          *   taskOutput object.
          */
-        console.log($("input[name=Options]:checked").val());
+
         console.log($("input[name=Options]:checked"));
         return $("input[name=Options]:checked").val(); 
     },
