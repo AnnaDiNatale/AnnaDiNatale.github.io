7558fc8fece39439363fdb65ba18a66067303f33
Anna Di Natale <47451033+AnnaDiNatale@users.noreply.github.com>
2020-01-21 14:50:01 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,11 +120,22 @@ function addHiddenField(form, name, value) {
     form.append(input);
 }
 
-function download(text, name, type) {
-  var a = document.getElementById("a");
-  var file = new Blob([text], {type: type});
-  a.href = URL.createObjectURL(file);
-  a.download = name;
+function download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
 }
 
 function submitHIT() {
