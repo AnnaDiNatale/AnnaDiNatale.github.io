9b860fa042dc68d70181c704b1fca41d2673f841
Anna Di Natale <47451033+AnnaDiNatale@users.noreply.github.com>
2020-01-23 12:29:48 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,7 +255,7 @@ $(document).ready(function() {
             state.taskOutputs = {};
         }
         custom.loadTasks(config.meta.numSubtasks).done(function(taskInputs) {
-            state.taskInputs = ['plane aircraft','fact do', 'whatever if','4 5','6 7','8 9','10 11','12 13','14 15','16 17'];
+            state.taskInputs = ['plane aircraft','compulsory inexorable', 'easy simple','mistake wrong','dot point','belly wood','mularly individual','book wrong','human individual','neck nape'];
             populateMetadata(config);
             setupButtons(config);
         });
