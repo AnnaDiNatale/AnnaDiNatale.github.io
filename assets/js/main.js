eacd9859a18f1b9de0701ba0daaa55c3906afb67
Anna Di Natale <47451033+AnnaDiNatale@users.noreply.github.com>
2020-01-31 10:31:18 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,7 +69,9 @@ function updateTask() {
 
 function nextTask() {
     console.log('button');
-    console.log(state.taskIndex)
+    console.log(state.taskIndex);
+    console.log(config.meta.numSubtasks);
+    console.log(state.taskIndex == config.meta.numSubtasks - 1)
     if (state.taskIndex < config.meta.numSubtasks - 1) {
         saveTaskData();
         var err = custom.validateTask(getTaskInputs(state.taskIndex), state.taskIndex, getTaskOutputs(state.taskIndex));
