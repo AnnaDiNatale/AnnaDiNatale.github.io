9da5f8a8382b061b51d704ea8f3bcef7874355e0
Anna Di Natale <47451033+AnnaDiNatale@users.noreply.github.com>
2020-01-31 11:56:27 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,9 +62,9 @@ if (state.taskIndex == 0) {
         $("#final-task-fields").css("display", "none");
         $("#congrats").css("display", "none");
     } else if (state.taskIndex == config.meta.numSubtasks) {
-        console.log($("#submit-button"));
         $("last").css("display", "flex");
-        $("last").css("display", "block");
+        $("#last").css("display", "block");
+        console.log($("#last"));
         $("#submit-button").removeClass("disabled"); 
         $("#final-task-fields").css("display", "block");
         $("#experiment").css("display", "none");
