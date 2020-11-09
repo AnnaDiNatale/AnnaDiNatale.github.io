6135bc82c14404dadebd630133720850f7592a0e
Anna Di Natale <47451033+AnnaDiNatale@users.noreply.github.com>
2020-11-09 13:13:12 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -281,7 +281,7 @@ $(document).ready(function() {
         }
         custom.loadTasks(config.meta.numSubtasks).done(function(taskInputs) {
         $.getJSON("questions.json").done(function(data) { questions=data;
-            state.taskInputs = questions.onehundredtwentytwo;
+            state.taskInputs = questions.onehundredtwentythree;
             populateMetadata(config);
             setupButtons(config);
         });
