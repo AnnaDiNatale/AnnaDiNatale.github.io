4fda5f54dac1acdb7d1f9fa9485131f62be2d1f2
Anna Di Natale <47451033+AnnaDiNatale@users.noreply.github.com>
2021-07-30 17:53:06 +0200

    Update main.js

--- leia/assets/js/main.js
+++ leia/assets/js/main.js
@@ -281,7 +281,7 @@ $(document).ready(function() {
         }
         custom.loadTasks(config.meta.numSubtasks).done(function(taskInputs) {
         $.getJSON("questions.json").done(function(data) { questions=data;
-            state.taskInputs = questions.twentysix;
+            state.taskInputs = questions.twentyseven;
             populateMetadata(config);
             setupButtons(config);
         });
